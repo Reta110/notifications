1ea757ca41f2f85b763606dcb8a78892692eb13e
Rafael Torrealba <rafaeltorrealba6@gmail.com>
2018-11-13 18:52:56 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\User;
+use App\Notifications\TaskCompleted;
+use App\Events\eventTrigger;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//DOING THE EVENT LISTENER
+Route::get('/', function () {
+
+	User::find(1)->notify(new TaskCompleted);
+
+	return view('welcome');
+});
+
+//DOING THE BROADCASTING
+Route::get('/alert', function () {
+
+	return view('welcome');
+});
+
+Route::get('/trigger', function () {
+
+	event(new eventTrigger());
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
